e18ad0a53502d7f8759a8e19687353a48441ca00
CaptainSherbert <captainsherbert179@gmail.com>
2025-09-24 16:56:16 -0400

    why are you reading this? why are you reading this?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MilesTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MilesTeleop.java
@@ -58,7 +58,7 @@ public class MilesTeleop extends OpMode {
         bottomRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 //To whomever shall be reading this in the near future... i got nothing. GAH CAN THIS THING WORKKKK!!!1!!
-
+// GAAHAAHAHGH PLEASE WORK THIS TIME!!!111!!
     }
 
     // Runs every frame
