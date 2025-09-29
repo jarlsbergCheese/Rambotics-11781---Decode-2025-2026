438d695457cecd93104edbebe30494f3fa2fe56b
yeah-caleb <caleb.meeker@mvschool.com>
2025-09-29 16:37:43 -0400

    hoco ready

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuningAuto.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.subsytems.Drivetrain;
+
+public class tuningAuto extends OpMode {
+
+    Drivetrain drivetrain;
+
+
+    @Override
+    public void init() {
+        drivetrain = new Drivetrain(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+
+        drivetrain.gamePadInputs(gamepad1, 0);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/Drivetrain.java
@@ -17,7 +17,16 @@ public class Drivetrain {
     public float topLeftPower;
     public float bottomRearPower;
 
-    Drivetrain(HardwareMap hwMap)
+    double xt;
+    double yt;
+
+    double x;
+    double y;
+    double rx;
+
+    double max;
+
+    public Drivetrain(HardwareMap hwMap)
     {
         topRightMotor = hwMap.get(DcMotorEx.class, "topRightMotor");
         topLeftMotor = hwMap.get(DcMotorEx.class, "topLeftMotor");
@@ -33,10 +42,36 @@ public class Drivetrain {
         bottomLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    public void gamePadInputs(Gamepad gmpad)
+    public void gamePadInputs(Gamepad gmpad, float heading)
     {
 
+    xt = gmpad.left_stick_x;
+    yt = -gmpad.left_stick_y;
+    rx = gmpad.right_stick_x;
 
+    x = (xt * Math.cos(heading) - (yt * Math.sin(heading)));
+    y = (xt * Math.sin(heading) + (yt * Math.cos(heading)));
+
+    double topRightPower = y+x+rx;
+    double topLeftPower = y-x-rx;
+    double bottomRightPower = y-x+rx;
+    double bottomLeftPower = y+x-rx;
+
+    max = Math.max(Math.abs(topLeftPower), Math.abs(topRightPower));
+    max = Math.max(max, Math.abs(bottomLeftPower));
+    max = Math.max(max, Math.abs(bottomRightPower));
+
+    if (max > 1.0) {
+        topLeftPower  /= max;
+        topRightPower /= max;
+        bottomRightPower  /= max;
+        bottomLeftPower  /= max;
+    }
+
+    topRightMotor.setPower(topRightPower*0.9);
+    topLeftMotor.setPower(topLeftPower*0.9);
+    bottomRightMotor.setPower(bottomRightPower*0.9);
+    bottomLeftMotor.setPower(bottomLeftPower*0.9);
 
     }
 
